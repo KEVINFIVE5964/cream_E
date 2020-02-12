8769636490570cf39c08a150bc56c6299cc141b0
KEVINFIVE5964 <60974883+KEVINFIVE5964@users.noreply.github.com>
2020-02-12 23:38:13 +0900

    Add files via upload

---
+++ cream_E.py.py
@@ -0,0 +1,37 @@
+#By Ineub
+#Edit only
+#Undeployable, revised and left undeployable
+
+
+import discord
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("no god plese no!!!!! no!!!!!")
+    game = discord.Game("이늡 바부")
+    await client.change_presence(status=discord.Status.idle, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("크림 안녕"):
+        await message.channel.send("안녕~!")
+    if message.content.startswith("크림 명령어"):
+        await message.channel.send("```cream_E 명령어:크림 안녕,크림 바바, 크림 바부, 크림 찬양해(하면 좋은일이 있을지도...?)```")
+    if message.content.startswith("크림 욕해봐"):
+        await message.channel.send("||ㅆ1발||")
+    if message.content.startswith("크림 찬양해"):
+        await message.channel.send("나도 널 찬양한다!!!")
+    if message.content.startswith("크림 바바"):
+        await message.channel.send("잘하...힝")
+    if message.content.startswith("크림 바부"):
+        await message.channel.send("응 자기소개 잘~한다")
+
+
+client.run("Njc2Njg4MDA1NDE4OTc1MjUy.XkO-ag.chR1TSR-4R7Un_Y0fPqVznnp5Wk")
